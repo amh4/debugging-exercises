f92f8f3df6120e5a60e9ef5b25c2a9863239c7db
abodian <ajnbodian@gmail.com>
2022-12-07 15:11:03 +0000

    Completed first debugging. Continue with Discovery Debugging

---
+++ lib/encode.rb
@@ -0,0 +1,26 @@
+def encode(plaintext, key)
+  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  ciphertext_chars = plaintext.chars.map do |char|
+    (65 + cipher.find_index(char).to_i).chr
+  end
+  print ciphertext_chars.join
+end
+
+def decode(ciphertext, key)
+  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  plaintext_chars = ciphertext.chars.map do |char|
+    cipher[65 - char.ord]
+  end
+  print plaintext_chars.join
+end
+
+encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
+
+# Intended output:
+#
+# > encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+# => "EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL"
+#
+# > decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
+# => "theswiftfoxjumpedoverthelazydog"
